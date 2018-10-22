54f74c7764de97a612051c5deff7a7a4d0aef2d2
Justyna Broniszewska <>
2018-10-22 15:30:50 +0200

    Token granting relayer deploy

--- universal-login-example/src/relayer/utils.js
+++ universal-login-example/src/relayer/utils.js
@@ -1,12 +1,15 @@
 import ethers from 'ethers';
 
-const waitForContractDeploy = async (providerOrWallet, contractJSON, transactionHash, tick = 1000) => {
+const sleep = (ms) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+const waitForContractDeploy = async (providerOrWallet, contractJSON, tansactionHash, tick = 1000) => {
   const provider = providerOrWallet.provider ? providerOrWallet.provider : providerOrWallet;
   const abi = contractJSON.interface;
-  let receipt = await provider.getTransactionReceipt(transactionHash);
+  let receipt = await provider.getTransactionReceipt(tansactionHash);
   while (!receipt) {
     sleep(tick);
-    receipt = await provider.getTransactionReceipt(transactionHash);
+    receipt = await provider.getTransactionReceipt(tansactionHash);
   }
   return new ethers.Contract(receipt.contractAddress, abi, providerOrWallet);
 };
